e34e55f2a8761a6401f230a7558cf5f0ef32b598
Narcis13 <Narcis75@gmail.com>
2023-10-01 20:00:10 +0300

    viteeeeza

--- components/Register.vue
+++ components/Register.vue
@@ -1,11 +1,26 @@
+<script setup>
+let nume=ref('')
+let parola=ref('')
+let eAdmin = ref(false)
+
+function salveazaUtilizator(){
+  console.log("utilizator", nume.value,parola.value,eAdmin.value)
+  navigateTo("../place")
+}
+</script>
+
+
+
 <template>
+
    <div class="flex flex-center q-pa-md">
 
     <div class="q-gutter-y-md column flex-center" style="max-width: 300px">
         <h5>Inregistrare utilizator nou</h5>
-      <q-input clearable filled color="purple-12"  label="Label" />
-      <q-input clearable filled color="purple-12"  label="Label" />
-      <q-input clearable filled color="purple-12"  label="Label" />
+      <q-input v-model="nume" clearable filled color="purple-12"  label="Adresa mail" />
+      <q-input v-model="parola" clearable filled color="purple-12"  label="Parola"  type="password" hint="Minim 8 caractere"/>
+      <q-checkbox v-model="eAdmin" label="E administrator" color="teal" />
+      <q-btn color="purple" label="Salveaza !"  @click="salveazaUtilizator"/>
     </div>
    
   </div>
